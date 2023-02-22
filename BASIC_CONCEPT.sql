03365983c3f7807322fbd7c84f199205c64f08f2
KrishRohith16 <krishrohith2000@gmail.com>
2023-02-22 08:36:13 +0530

    What is RDBMS ?

--- BASIC_CONCEPT.sql
+++ BASIC_CONCEPT.sql
@@ -1,2 +1,27 @@
 CREATE DATABASE BASIC_CONCEPT;
 USE BASIC_CONCEPT;
+
+-- WHAT IS A RDBMS ?
+/*
+1)	RDBMS stands for Relational Database Management System.
+2)	RDBMS is a program used to maintain a relational database.
+3)	RDBMS is the basis for all modern database systems such as 
+4)	MySQL, Microsoft SQL Server, Oracle, and Microsoft Access. 
+5)	RDBMS uses SQL queries to access the data in the database.
+*/
+
+-- SQL COMMANDS : DDL , DML , DCL , TCL , DQL
+
+-- DATA TYPES
+
+-- CONSTRAINTS
+
+-- NORMALIZATION IN SQL
+
+-- OPERATORS
+
+-- CASE STATEMENT
+
+-- IMPORTANT SQL CLAUSE
+
+-- INNER JOIN
